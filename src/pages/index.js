d3f34dce8035e56fe5298817c925e71a05010c5e
Joaquim <juakacc@gmail.com>
2020-06-13 22:12:48 -0300

    clear project

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,11 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Sejam bem-vindos ao meu site pessoal</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
