6fed4d441608f1f474ac3b918e5d6d98e3b81890
Joaquim <juakacc@gmail.com>
2020-06-15 22:49:32 -0300

    Adjusts to responsive

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,15 +9,9 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle="<Joaquim Aníbal Rocha Neto />" />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main className="site-container">{children}</main>
-      </div>
+
+      <main className="site-container">{children}</main>
+
       <footer
         style={{
           background: "#36cc1f",
